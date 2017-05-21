9ce107b3dc64441d3bdd20f48fe2e6e2eb101eac
Robert Ang <angrobertsh@gmail.com>
2017-05-21 13:26:22 -0700

    it's a column chart!

--- frontend/components/graphs/bar_chart.jsx
+++ frontend/components/graphs/bar_chart.jsx
@@ -8,6 +8,40 @@ class BarChart extends React.Component{
   }
 
   componentDidMount(){
+    // const data = this.props.data;
+    // const labels = Object.keys(data.votes);
+    // const dataElectoralDemocratMap = labels.map((state) => (data.votes[state].electoral.democrat));
+    // const dataElectoralRepublicanMap = labels.map((state) => (data.votes[state].electoral.republican));
+    // const dataPopularDemocratMap = labels.map((state) => (data.votes[state].popular.democrat));
+    // const dataPopularRepublicanMap = labels.map((state) => (data.votes[state].popular.republican));
+    // const dataPopularOtherMap = labels.map((state) => (data.votes[state].popular.other));
+    //
+    // const width = 960,
+    //     barHeight = 20;
+    //
+    // const x = d3.scaleLinear()
+    //     .domain([0, d3.max(dataPopularDemocratMap.concat(dataPopularRepublicanMap).concat(dataPopularOtherMap))])
+    //     .range([0, width]);
+    //
+    // const chart = d3.select(".barchart")
+    //     .attr("width", width)
+    //     .attr("height", barHeight * labels.length);
+    //
+    // const bar = chart.selectAll("g")
+    //     .data(dataPopularDemocratMap)
+    //   .enter().append("g")
+    //     .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    //
+    // bar.append("rect")
+    //     .attr("width", x)
+    //     .attr("height", barHeight - 1);
+    //
+    // bar.append("text")
+    //     .attr("x", function(d) { return x(d) - 3; })
+    //     .attr("y", barHeight / 2)
+    //     .attr("dy", ".35em")
+    //     .text(function(d) { return d; });
+
     const data = this.props.data;
     const labels = Object.keys(data.votes);
     const dataElectoralDemocratMap = labels.map((state) => (data.votes[state].electoral.democrat));
@@ -16,31 +50,34 @@ class BarChart extends React.Component{
     const dataPopularRepublicanMap = labels.map((state) => (data.votes[state].popular.republican));
     const dataPopularOtherMap = labels.map((state) => (data.votes[state].popular.other));
 
-    const width = 420,
-        barHeight = 20;
+    const width = 960,
+        height = 500,
+        barWidth = width / labels.length;
 
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(dataPopularDemocratMap.concat(dataPopularRepublicanMap).concat(dataPopularOtherMap))])
-        .range([0, width]);
+    const y = d3.scaleLinear()
+        .range([height, 0])
+        .domain(([0, d3.max(dataPopularDemocratMap)]));
 
     const chart = d3.select(".barchart")
         .attr("width", width)
-        .attr("height", barHeight * labels.length);
+        .attr("height", height);
 
     const bar = chart.selectAll("g")
         .data(dataPopularDemocratMap)
       .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+        .attr("transform", (d, i) => ("translate(" + i * barWidth + ",0)"));
 
     bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1);
+        .attr("y", (d) => ( y(d) ))
+        .attr("height", (d) => ( height - y(d)) )
+        .attr("width", barWidth - 1);
 
     bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d; });
+        .attr("x", barWidth / 2)
+        .attr("y", (d) => ( y(d) + 3 ))
+        .attr("dy", ".75em")
+        .text((d) => ( d ));
+
   }
 
   render(){
